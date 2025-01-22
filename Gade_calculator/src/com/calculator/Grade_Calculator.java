8129523e6e5f66ea001c64542941f20355db2e82
vaishnavibaurasi <111147334+vaishnavibaurasi@users.noreply.github.com>
2025-01-22 15:02:28 +0530

    Add files via upload

---
+++ Gade_calculator/src/com/calculator/Grade_Calculator.java
@@ -0,0 +1,49 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class Grade_Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+
+            if (marks[i] < 0 || marks[i] > 100) {
+                System.out.println("Invalid marks. Please enter a value between 0 and 100.");
+                i--; 
+            } else {
+                totalMarks += marks[i];
+            }
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade;
+
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
